e00515bd7b6257fb9f56f945042522d5a31d4a31
murph666 <murph666@github.com>
2020-12-18 16:53:39 +0300

    Update network0.2.py

--- nn/epoh1/network0.2.py
+++ nn/epoh1/network0.2.py
@@ -49,8 +49,59 @@ class NeuralNetwork:
 
     def train(self, data, all_y_trues):
         learn_rate = 0.1
-        epochs = 100
+        epochs = 1000
+        y_pred = 0
 
-        for epoh in range(epochs):
-            pass
+        for epoch in range(epochs):
+            for x, y_true in zip(data, all_y_trues):
+                out_h1 = sigmoid(self.w1 * x[0] + self.w2 * x[1] + self.b1)
+                out_h2 = sigmoid(self.w3 * x[0] + self.w4 * x[1] + self.b1)
+                out = sigmoid(self.w5 * out_h1 + self.w6 * out_h2 + self.b1)
+                y_pred = out
 
+                d_L_d_ypred = -2 * (y_true - y_pred)
+
+                d_ypred_d_w5 = out_h1 * derivative_of_sigmoid(out)
+                d_ypred_d_w6 = out_h2 * derivative_of_sigmoid(out)
+                d_ypred_d_b3 = derivative_of_sigmoid(out)
+
+                d_ypred_d_h1 = self.w5 * derivative_of_sigmoid(out)
+                d_ypred_d_h2 = self.w6 * derivative_of_sigmoid(out)
+
+                d_h1_d_w1 = x[0] * derivative_of_sigmoid(out_h1)
+                d_h1_d_w2 = x[1] * derivative_of_sigmoid(out_h1)
+                d_h1_d_b1 = derivative_of_sigmoid(out_h1)
+
+                d_h2_d_w3 = x[0] * derivative_of_sigmoid(out_h2)
+                d_h2_d_w4 = x[1] * derivative_of_sigmoid(out_h2)
+                d_h2_d_b2 = derivative_of_sigmoid(out_h2)
+
+                # --- Update weights and biases
+                # Neuron h1
+                self.w1 -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_w1
+                self.w2 -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_w2
+                self.b1 -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_b1
+
+                # Neuron h2
+                self.w3 -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_w3
+                self.w4 -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_w4
+                self.b2 -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h2_d_b2
+
+                # Neuron o1
+                self.w5 -= learn_rate * d_L_d_ypred * d_ypred_d_w5
+                self.w6 -= learn_rate * d_L_d_ypred * d_ypred_d_w6
+                self.b3 -= learn_rate * d_L_d_ypred * d_ypred_d_b3
+
+                if epoch % 10 == 0:
+                    y_preds = np.apply_along_axis(self.feed_forward, 1, data)
+                    loss = mse_loss(all_y_trues, y_preds)
+                    print("Epoch %d loss: %.3f" % (epoch, loss))
+
+
+data = np.array([[-2, -1], [25, 6], [17, 4], [-15, -6]])
+all_y_trues = np.array([1, 0, 0, 1])
+
+network = NeuralNetwork()
+network.train(data, all_y_trues)
+emily = np.array([-7, -3])
+print("Emily: %.3f" % network.feed_forward(emily))
